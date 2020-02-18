dcd1590f0449d3d34fb6b28ffb49783830f2208d
Fenech <marine.fenech00@gmail.com>
2020-02-17 20:00:03 -0500

    MF - Animations header et nav

--- mjs/anim_Intro.mjs
+++ mjs/anim_Intro.mjs
@@ -1,4 +1,24 @@
-window.addEventListener("load", function () {
-    var titre = document.querySelector("header h1");
+/*let titre = document.querySelector("header h1");
+let sectionAnim = document.querySelector("header");
+let nav = document.getElementById("menu");
+couleurDébut: rgb(105, 108, 138);
+couleurFin : rgb(254, 254, 255);
 
- });
+
+ @keyframes animTitre {
+    from {
+        nav.style.opacity = "0";
+        sectionAnim.style.position = "absolute";
+        sectionAnim.style.backgroundColor = "#fff";
+        sectionAnim.style.height="100vh";
+        sectionAnim.style.width="100vh";
+        titre.style.zIndex ="100";
+    }
+
+    100
+    
+
+}
+
+
+window.addEventListener("load", animTitre);*/
