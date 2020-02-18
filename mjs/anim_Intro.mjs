042664a74f47225d4320c75f31f6cd2473ce0686
Fenech <marine.fenech00@gmail.com>
2020-02-18 15:05:42 -0500

    MF - animations js

--- mjs/anim_Intro.mjs
+++ mjs/anim_Intro.mjs
@@ -1,81 +1,42 @@
- /*@keyframes animTitre {
-    from {
-        nav.style.opacity = "0";
-        sectionAnim.style.position = "absolute";
-        sectionAnim.style.backgroundColor = "#fff";
-        sectionAnim.style.height="100vh";
-        sectionAnim.style.width="100vh";
-        titre.style.zIndex ="100";
+export class anim_header {
+    constructor(elmHeader) {
+        this.elmHeader = elmHeader
     }
 
-    100
-}*/
+    animHeader() {
+        let elmHeader = document.querySelector("header");
 
-let titre = document.querySelector("header h1");
-let sectionAnim = document.querySelector("header");
-let nav = document.getElementById("menu");
-
-var animHeader;
-var animTitre;
-var animNav;
-
-titre.style.opacity = "0";
-nav.style.opacity = "0";
-
-animHeader = () => {
-    console.log("allo");
-    sectionAnim.animate([
-        {
-            transform : "scale(10)",
-            backgroundColor : "#fff"
-        },
-        {
-            transform : "scale(1)",
-            backgroundColor : "rgb(105, 108, 138)"
-        }
-    ],
-    {
-        duration: 5000
-    });
-    /*animHeader.addEventListener("animationend", function(){
-        sectionAnim.style.animationFillMode = "forwards";
-    })*/
-
-    setTimeout(animTitre, 3000);
+        elmHeader.classList.add('animHeader');
+    }
+    
 }
 
-animTitre = () => {
-    console.log("allo");
-    sectionAnim.animate([
-        {
-            opacity : "0",
-            color : "rgb(105, 108, 138)"
-        },
-        {
-            opacity: "100",
-            color : "white"
-        }
-    ],
-    {
-        duration: 2000
-    });
-    setTimeout(animNav, 3000);
+export class anim_titre {
+    constructor(elmTitre) {
+        this.elmTitre = elmTitre
+    }
+
+
+    animTitre() {
+        let elmTitre = document.querySelector("header h1");
+        
+        elmTitre.style.opacity = "0";
+
+        elmTitre.classList.add('animTitre');
+    }
 }
 
-animNav = () => {
-    console.log("allo");
-    sectionAnim.animate([
-        {
-            opacity : "0"
-        },
-        {
-            opacity: "100"
-        }
-    ],
-    {
-        duration: 2000
-    });
+export class anim_Nav {
+    constructor(elmNav) {
+        this.elmNav = elmNav
+    }
+
+    animNav() {
+        let elmNav = document.getElementById("menu");
+
+        elmNav.style.opacity = "0";
+
+        elmNav.classList.add('animMenu');
+    }
+    
 }
-
-
-window.addEventListener("load", animHeader);
--- mjs/main.mjs
+++ mjs/main.mjs
@@ -1,5 +1,21 @@
+import {anim_header} from "./anim_Intro.mjs";
+import {anim_titre} from "./anim_Intro.mjs";
+import {anim_Nav} from "./anim_Intro.mjs";
 import { Slider } from "./slider.mjs";
 import { config_slider } from "./sliderConfig.mjs";
+
+let elmHeader = document.querySelector("header");
+const animHeader = new anim_header(elmHeader);
+animHeader.animHeader();
+
+let elmTitre = document.querySelector("header h1");
+const animIntro = new anim_titre(elmTitre);
+animIntro.animTitre();
+
+let elmNav = document.querySelector("nav");
+const animNav = new anim_Nav(elmNav);
+animNav.animNav();
+
 const elmSlider = document.querySelector('.mon-slider')
 console.log(elmSlider.tagName)
 let mon_slider = new Slider(elmSlider, config_slider)
