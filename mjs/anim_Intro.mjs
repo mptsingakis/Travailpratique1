9b2e16bc32d7ec9324aa074cbab2ec61dfb4090d
Marine Fenech <marinefenech@MacBook-Pro-de-Marine.local>
2020-02-17 23:41:02 -0500

    MF - Animation js

--- mjs/anim_Intro.mjs
+++ mjs/anim_Intro.mjs
@@ -1,11 +1,4 @@
-/*let titre = document.querySelector("header h1");
-let sectionAnim = document.querySelector("header");
-let nav = document.getElementById("menu");
-couleurDébut: rgb(105, 108, 138);
-couleurFin : rgb(254, 254, 255);
-
-
- @keyframes animTitre {
+ /*@keyframes animTitre {
     from {
         nav.style.opacity = "0";
         sectionAnim.style.position = "absolute";
@@ -16,9 +9,73 @@ couleurFin : rgb(254, 254, 255);
     }
 
     100
-    
+}*/
 
+let titre = document.querySelector("header h1");
+let sectionAnim = document.querySelector("header");
+let nav = document.getElementById("menu");
+
+var animHeader;
+var animTitre;
+var animNav;
+
+titre.style.opacity = "0";
+nav.style.opacity = "0";
+
+animHeader = () => {
+    console.log("allo");
+    sectionAnim.animate([
+        {
+            transform : "scale(10)",
+            backgroundColor : "#fff"
+        },
+        {
+            transform : "scale(1)",
+            backgroundColor : "rgb(105, 108, 138)"
+        }
+    ],
+    {
+        duration: 5000
+    });
+    /*animHeader.addEventListener("animationend", function(){
+        sectionAnim.style.animationFillMode = "forwards";
+    })*/
+
+    setTimeout(animTitre, 3000);
+}
+
+animTitre = () => {
+    console.log("allo");
+    sectionAnim.animate([
+        {
+            opacity : "0",
+            color : "rgb(105, 108, 138)"
+        },
+        {
+            opacity: "100",
+            color : "white"
+        }
+    ],
+    {
+        duration: 2000
+    });
+    setTimeout(animNav, 3000);
+}
+
+animNav = () => {
+    console.log("allo");
+    sectionAnim.animate([
+        {
+            opacity : "0"
+        },
+        {
+            opacity: "100"
+        }
+    ],
+    {
+        duration: 2000
+    });
 }
 
 
-window.addEventListener("load", animTitre);*/
+window.addEventListener("load", animHeader);
